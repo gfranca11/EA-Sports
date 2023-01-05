bb6ebfbeeda4898da73412b83318c11e7f619ec7
gfranca11 <geo,palmeiras300@gmail.com>
2023-01-05 16:20:37 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const mobileButton = document.querySelector('.mobileButton')
+function handleOpenMobileButton(){
+    mobileButton.style.display = "flex"
+
+}
+function handleCloseMobileButton(){
+    mobileButton.style.display = "none"
+
+}
